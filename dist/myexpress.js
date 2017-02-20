1b03590d0115491333054c0af9662621c733085e
Parvez.Ali <Parvez.Ali@dh.com>
2017-02-20 12:47:46 +0530

    Whoops again

--- dist/myexpress.js
+++ dist/myexpress.js
@@ -20,7 +20,7 @@ app.use('/api',	ApiRouter);
 app.use('/auth', AuthRouter);
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
