1afc79f3df978789e8b03f971fea75a7544fb389
codesmith21 <rhtbhattar@gmail.com>
2017-03-13 01:29:27 +0530

    Mongo DB URL modification

--- server/config/production.js
+++ server/config/production.js
@@ -3,7 +3,7 @@ module.exports = {
   seed: false,
   appHostName: "http://talenttrack.azurewebsites.net",
   db: {
-    url: 'mongodb://trackifymongo:T0IG3WDmzSV0gimJWQmRUIZDndB8E1lkVf856HXWIjznu5ptYtYky1IKosy3BN0joT6hWS1W4mn2VWOIdRIYDw==@trackifymongo.documents.azure.com:10250/?ssl=true',
+    url: 'mongodb://trackifymongo:T0IG3WDmzSV0gimJWQmRUIZDndB8E1lkVf856HXWIjznu5ptYtYky1IKosy3BN0joT6hWS1W4mn2VWOIdRIYDw==@trackifymongo.documents.azure.com:10250/trackify?ssl=true',
   }
 };
 
