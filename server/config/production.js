9352bef862c029c9849cbbb4f1b850081535abc9
Prasanna Kumar <jpkbeddu@gmail.com>
2017-03-14 17:11:13 +0530

    Fixes escaping invalid string in mongo connect url

--- server/config/production.js
+++ server/config/production.js
@@ -3,7 +3,9 @@ module.exports = {
   seed: false,
   appHostName: "http://talenttrack.azurewebsites.net",
   db: {
-    url: 'mongodb://trackifymongo:T0IG3WDmzSV0gimJWQmRUIZDndB8E1lkVf856HXWIjznu5ptYtYky1IKosy3BN0joT6hWS1W4mn2VWOIdRIYDw==@trackifymongo.documents.azure.com:10250/trackify?ssl=true',
+    url: "mongodb://52.230.28.150:27017/trackify",
+    username: 'trackify',
+    password: 'P@assword1234'
   }
 };
 
