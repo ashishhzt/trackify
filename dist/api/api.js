ad6edf08765ef9de8a3126f6912d1b6bfda91be5
Parvez Ali <Parvez.Ali@harlandfs.com>
2017-03-15 22:49:59 +0530

    New Job push half done

--- dist/api/api.js
+++ dist/api/api.js
@@ -1,6 +1,5 @@
 import router			from 'express';
 import UserRouter		from './users/user.router';
-import MembersRouter	from './members/members.router';
 import JobsRouter       from './jobs/jobs.router';
 import CandidateRouter  from './candidate/candidate.router';
 import NewJobRouter     from './newjob/newjob.router'
@@ -8,7 +7,6 @@ import NewJobRouter     from './newjob/newjob.router'
 const ApiRouter = router();
 
 ApiRouter.use('/users',	UserRouter);
-ApiRouter.use('/members', MembersRouter);
 ApiRouter.use('/jobs', JobsRouter);
 ApiRouter.use('/candidate', CandidateRouter);
 ApiRouter.use('/newjob', NewJobRouter);
--- dist/api/newjob/newjob.controller.js
+++ dist/api/newjob/newjob.controller.js
@@ -1,54 +1,11 @@
 import Newjob from './newjob.model';
 import _    from 'lodash';
 
-export const params = (req, res, next, id) => {
-  Newjob.findById(id)
-    .exec()
-    .then((newjob) => {
-      if (!newjob) {
-        return res.status(400).send({ message: 'Newjob not found' });
-      } else {
-        req.newjob = newjob;
-        next();
-      }
-    }, next);
-};
-
-export const get = (req, res, next) => {
-  Newjob.find({})
-    .exec()
-    .then((newjobs) => {
-      res.send(newjobs);
-    }, next);
-};
-
-export const getOne = (req, res) => {
-  res.send(req.newjob);
-};
-
-export const post = (req, res, next) => {
+export const createJob = (req, res, next) => {
   var newNewjob = req.body;
 
   Newjob.create(newNewjob)
     .then((created) => {
       res.send(created);
-    }, next);
-};
-
-export const put = (req, res, next) => {
-  var {newjob, body} = req;
-
-  _.merge(newjob, body);
-
-  newjob.save()
-    .then((updated) => {
-      res.send(updated);
-    }, next);
-};
-
-export const del = (req, res, next) => {
-  req.newjob.remove()
-    .then((removed) => {
-      res.send(removed);
     }, next);
 };
